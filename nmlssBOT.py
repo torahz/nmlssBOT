8538ca7acace9d4e38e92f7f0763f4e851b8d375
torahz <rafaeltorahz@gmail.com>
2023-11-08 11:16:18 -0300

    Update nmlssBOT.py

--- nmlssBOT.py
+++ nmlssBOT.py
@@ -2,11 +2,11 @@ import json
 import random
 from nltk.chat.util import Chat, reflections
 
-arquivo_respostas = 'respostas.json' 
+arquivo_respostas = 'respostas.json'
 
 try:
   with open(arquivo_respostas) as arquivo:
-    respostas_aprendidas = json.load(arquivo)
+    respostas_aprendidas = json.load(arquivo)  
 except FileNotFoundError:
   respostas_aprendidas = {}
 
@@ -14,33 +14,32 @@ respostas_fixas = {
   "oi": ["olá", "Oi tudo bem?", "Eae"],
   "tudo bem?": ["Tudo ótimo", "Não poderia estar melhor"],
   "qual seu nome?": ["Meu nome é Bot", "Pode me chamar de Bot"],
-  "como você está?": ["Estou bem, obrigado por perguntar!", "Não poderia estar melhor!"]   
+  "como você está?": ["Estou bem, obrigado por perguntar!", "Não poderia estar melhor!"]
 }
 
 with open('dicptbrverb.txt') as arquivo:
   dicionario = json.load(arquivo)
 
 def busca_na_web(pergunta):
-  # Busca na web 
-  resposta = #busca e extrai resposta
+  
+  resposta = "" #busca na web e extrai resposta
   
   return resposta
 
 def busca_no_dicionario(pergunta):
 
-  #Busca no dicionário 
-  resposta = #busca palavras relacionadas e forma resposta 
-  
+  resposta = "" #busca palavras relacionadas e forma resposta
+
   return resposta
-  
+
 def responde(pergunta):
-  
+
   resposta = busca_na_web(pergunta)
-  
+
   if not resposta:
-  
+
     resposta = busca_no_dicionario(pergunta)
-    
+
     if not resposta:
     
       if pergunta in respostas_fixas:
@@ -51,9 +50,9 @@ def responde(pergunta):
         
       else:
         resposta = "Ainda não sei responder essa pergunta. Pode me ensinar?"
-  
-  return resposta 
 
-# resto do código...
+  return resposta
 
-# ao final, salva respostas aprendidas no arquivo
+# resto do código... 
+
+# salva respostas aprendidas
